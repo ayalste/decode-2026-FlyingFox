efa6d192f9599b8e5afa64c985138f6a0a1d2d96
eitankr200624 <eitankr200624@gmail.com>
2026-01-06 16:12:15 +0200

    fixed name of intake test file, added launcher test file, removed unecasery file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/test_everybot_intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/test_everybot_intake.java
@@ -19,31 +19,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="test_everybot_launcher", group="Teleop")
-public class test_everybot_launcher extends OpMode {
+@TeleOp(name="test_everybot_intake", group="Teleop")
+public class test_everybot_intake extends OpMode {
     // Declare OpMode members
     private ElapsedTime runtime = new ElapsedTime();
 
     // Drive motors
-    private DcMotor catapult1 = null;
-    private DcMotor catapult2 = null;
-
-
-    private enum CatapultModes {UP, DOWN, HOLD}
-    private CatapultModes pivotMode;
-
+    private DcMotor intakeMotor = null;
 
     @Override
     public void init() {
         telemetry.addData("Status", "Initializing");
 
         // Initialize hardware
-        catapult1 = hardwareMap.get(DcMotor.class, "catapult_motor1");
-        catapult2 = hardwareMap.get(DcMotor.class, "catapult_motor2");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake_Motor");
 
 
         // Set motor directions
-        catapult1.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
 
 
 
@@ -63,35 +56,22 @@ public class test_everybot_launcher extends OpMode {
     public void loop() {
 
 
-        // Gamepad 2: Launcher control
-
-        boolean catapultUpButton = gamepad2.right_bumper;
-        boolean catapultDownButton = gamepad2.right_trigger > 0.2;
-        if (catapultUpButton && catapultDownButton) {
-            catapultUpButton = false;
+        // Gamepad 2: intake control
+        if(gamepad2.a){
+            intakeMotor.setPower(1.0);
         }
-
-        if (catapultUpButton) {
-            pivotMode = CatapultModes.UP;
-            catapult1.setPower(-1.0);
-            catapult2.setPower(-1.0);
-        } else if (catapultDownButton) {
-            pivotMode = CatapultModes.DOWN;
-            catapult1.setPower(1);
-            catapult2.setPower(1);
-        } else {
-            pivotMode = CatapultModes.HOLD;
-            catapult1.setPower(0.2);
-            catapult2.setPower(0.2);
-            //Slight feed forward to keep catapult down while driving
+        else if (gamepad2.b){
+            intakeMotor.setPower(-1.0);
         }
-
+        else{
+            intakeMotor.setPower(0);
+        }
 
 
 
         // Telemetry
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("power:","volt:"+catapult1.getPower());
+        telemetry.addData("power:","volt:"+intakeMotor.getPower());
 
 
 
